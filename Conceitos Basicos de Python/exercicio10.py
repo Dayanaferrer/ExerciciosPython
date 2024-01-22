76adefa96d4e3e72f3cb246008c93b3d41f7213c
Dayanaferrer <dayanaferreira2@gmail.com>
2024-01-22 19:09:42 -0300

    exercicio reutiliza as variaves

---
+++ Conceitos Basicos de Python/exercicio10.py
@@ -0,0 +1,11 @@
+''' Faça um programa que utilize 4 variáveis como preferir e no final print uma mensagem amigável utilizando as variáveis criadas.
+Exemplos devariáveis: nome, idade, lugar, profissão...
+Exemplo de retorno: Olá Maria, prazer te conhecer. 
+Sou de São Paulo também e estou migrando de área. '''
+
+nome = "Ana Maria"
+idade = 30
+lugar = "Recife-PE"
+profissao = "Engenheira de Software"
+
+print(f"Olá {nome}, prazer te conhecer! Vejo que você tem {idade} anos e é uma {profissao} de {lugar}. Que incrível!")
