8d15541a282942f2d34bb5ee07ce75d847c916c5
Dayanaferrer <dayanaferreira2@gmail.com>
2024-01-22 17:54:33 -0300

    exercicio calculo da idade atual

---
+++ Conceitos Basicos de Python/exercicio2.py
@@ -0,0 +1,9 @@
+''' Peça ao usuário para informar o ano de nascimento.
+Em seguida, calcule e imprima a idade atual. '''
+
+ano_nascimento = int(input('Digite o ano de nascimento: '))
+ano_atual = int(input('Digite o ano atual: '))
+
+idade = ano_atual - ano_nascimento
+
+print(f'Sua idade é: {idade}')
