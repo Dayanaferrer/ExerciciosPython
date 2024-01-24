64b0ac79be96102251e5164003618a0093ee1fc5
Dayanaferrer <dayanaferreira2@gmail.com>
2024-01-24 20:25:37 -0300

    funcao com tres argumentos e sua soma

---
+++ Funcoes e extras/exercicio1.py
@@ -0,0 +1,8 @@
+''' Faça um programa, com uma função que necessite de três argumentos,
+ e que forneça a soma desses três argumentos'''
+
+def soma_numeros(x, y, z):
+    return x + y + z
+
+resultado = soma_numeros(0, 10, -5)
+print(f'O resultado da soma é: {resultado}')
